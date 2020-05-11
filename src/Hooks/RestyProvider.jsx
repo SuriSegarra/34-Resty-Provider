2225adf9605ae2a3a63dc98c1c34ce08f10981af
surelis segarra <sdarianne34@gmail.com>
2020-05-11 01:11:39 -0700

    created request component

--- src/Hooks/RestyProvider.jsx
+++ src/Hooks/RestyProvider.jsx
@@ -1,12 +1,32 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 const RestyContext = createContext();
 
 export const RestyProvider = ({ children }) => {
+const [url, setUrl] = useState('');
+const [method, setMethod] = useState('GET');
+const [body, setBody] = useState('');
 
+const onChange = ({ target }) => {
+if(target.name === 'url') setUrl(target.value);
+if(target.name === 'method') setMethod(target.value);
+if(target.name === 'body') setBody(target.value);
+}
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    makeRequest(url, method, body)
+    .then(res => console.log(res));
+}
     return (
-        <RestyContext.Provider value={}>
+        //context
+        <RestyContext.Provider value={ url, method, body, onChange, onSubmit }>
             {children}
         </RestyContext.Provider>
     );
 };
+
+export const useResty = () => {
+    const context = useContext(RestyContext);
+    return context;
+}
